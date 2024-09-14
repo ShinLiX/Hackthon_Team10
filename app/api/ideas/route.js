7b889fc0aae99ffbd18aa75156bac9d89009f136
zoe-zhi-liu <zoe.zhi.liu@qq.com>
2024-09-14 14:51:56 -0700

    Added the api to create an idea

--- app/api/ideas/route.js
+++ app/api/ideas/route.js
@@ -12,3 +12,17 @@ export async function GET() {
     return NextResponse.json({ message: 'Internal Server Error' }, { status: 500 });
   }
 }
+
+
+export async function POST(req) {
+  try {
+    const client = await clientPromise;
+    const db = client.db('admin');
+    const idea = await req.json(); // Extract idea data from request body
+    const result = await db.collection('ideas').insertOne(idea);
+    return NextResponse.json({ message: 'Idea created successfully', id: result.insertedId });
+  } catch (e) {
+    console.error(e);
+    return NextResponse.json({ message: 'Internal Server Error' }, { status: 500 });
+  }
+}
