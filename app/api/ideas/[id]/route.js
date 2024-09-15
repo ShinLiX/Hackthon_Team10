e8e358a428dc40cbe96a925846a0ed377dda58dd
sora11023539 <sora11023539@gmail.com>
2024-09-14 17:38:25 -0700

    fix: using object id

--- app/api/ideas/[id]/route.js
+++ app/api/ideas/[id]/route.js
@@ -1,13 +1,18 @@
 import { NextResponse } from 'next/server';
 import clientPromise from '@/lib/mongodb';
+import { ObjectId } from 'mongodb';
 
 export async function GET(req, { params }) {
   const { id } = params;
 
+  if (!ObjectId.isValid(id)) {
+    return NextResponse.json({ message: 'Invalid ID' }, { status: 400 });
+  }
+
   try {
     const client = await clientPromise;
     const db = client.db('hackathon_team_10');
-    const idea = await db.collection('ideas').findOne({ _id: id });
+    const idea = await db.collection('ideas').findOne({ _id: new ObjectId(id) });
 
     if (!idea) {
       return NextResponse.json({ message: 'Idea not found' }, { status: 404 });
